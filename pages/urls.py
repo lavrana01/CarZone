6cef6567740f56d406f4b4afb4c1c74102c9213a
lav <lavrana01@gmail.com>
2021-08-04 12:17:47 +0530

    created pages and configured navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
+    
 ]
