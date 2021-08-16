04f2993209aa20795eed11d2420596eef988276c
lav <lavrana01@gmail.com>
2021-08-16 19:09:14 +0530

    changes

--- cars/urls.py
+++ cars/urls.py
@@ -3,6 +3,8 @@ from cars import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.cars, name= 'cars')
+    path('', views.cars, name= 'cars'),
+    path('<int:id>',views.car_detail, name='car_detail'),
+    path('search', views.search, name='search'),
    
 ]
